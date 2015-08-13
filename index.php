c2ed3b3d23291ff199a832dca938198593ea7b92
Alex <daa0006@auburn.edu>
2015-08-13 16:11:35 +0000

    v 0.0.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>dank may-mays</title>
+
+        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    </head>
+    
+    <body>
+        <p>
+        <?php
+            echo "php test";
+        ?>
+        </p>
+    </body>
+</html>
